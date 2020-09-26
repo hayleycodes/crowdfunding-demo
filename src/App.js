7319b5a5b9872466a5f4c56844bd6067e2614150
Hayley van Waas <hayleyavw@gmail.com>
2020-09-26 08:53:56 +0800

    Uses data from API

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Nav />
 
         <Switch>
-          <Route path="/project">
+          <Route path="/project/:id">
             <ProjectPage />
           </Route>
           <Route path="/">
